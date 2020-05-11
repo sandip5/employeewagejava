fe134f93d54c118378b16dd0c7810024b6cec682
“sandip5” <singhsandeep.singh97@gmail.com>
2020-05-11 08:14:36 +0530

    UC One employee attendence

--- EmpWageAttendance.java
+++ EmpWageAttendance.java
@@ -1,11 +1,13 @@
-public class EmpWageAttendance {
-	public static void main(String[] args){
-	int IS_PRESENT=1;
-	double empCheck=(int)(Math.random()*10)%2;
-	if (empCheck == IS_PRESENT)
-		System.out.println("Employee is present");
-	else
-		System.out.println("Employee is absent");	
+public class EmpWageAttendance 
+{
+	public static void main(String[] args)
+	{
+		int IS_PRESENT=1;
+		double empCheck=(int)(Math.random()*10)%2;
+		if (empCheck == IS_PRESENT)
+			System.out.println("Employee is present");
+		else
+			System.out.println("Employee is absent");	
 	}
 }
 
