c5e0028ec483e3aab91f663a310f12f365712e84
“sandip5” <singhsandeep.singh97@gmail.com>
2020-05-11 11:56:20 +0530

    Calculate Monthly Wage

--- EmpWageAttendance.java
+++ EmpWageAttendance.java
@@ -2,29 +2,33 @@ import java.util.Random;
 
 public class EmpWageAttendance
 {
-        public static void main(String[] args)
-        {
-        	    Random rand = new Random();
-                int IS_PART_TIME=1;
-                int IS_FULL_TIME=2;
-                int EMP_RATE_PER_HRS=20;
-                int salary;
-                int EMP_HRS;
-                int empCheck=rand.nextInt(3);
-                if (empCheck == IS_FULL_TIME)
-                {
-                                EMP_HRS=8;
-                }
-                else if(empCheck == IS_PART_TIME)
-                {
-                                EMP_HRS=4;
-                }
-                else
-                {
-                                EMP_HRS=0;
-                }
-        salary=(EMP_RATE_PER_HRS*EMP_HRS);
-        System.out.println(salary);
-        }
+	public static void main(String[] args)
+	{
+		Random rand = new Random();
+		int IS_FULL_TIME=1;
+		int IS_PART_TIME=2;
+		int EMP_RATE_PER_HRS=20;
+		int NUM_WORK_DAYS=20;
+		int totalSalary=0;
+		int salary=0;
+		int EMP_HRS = 0;
+		for(int day =1;day<=NUM_WORK_DAYS;day++)
+		{	
+			int empCheck=rand.nextInt(3);
+			switch (empCheck) {
+			case 1:
+				EMP_HRS=8;
+				break;
+			case 2:
+				EMP_HRS=4;
+				break;
+			default:
+				EMP_HRS=0;	
+			}
+			salary=(EMP_RATE_PER_HRS*EMP_HRS);	
+			totalSalary=(totalSalary+salary);
+		}
+		System.out.println(totalSalary);
+	}
 }
 
