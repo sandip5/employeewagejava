afe9de697d349549a39a9dd5d0a05841e73e1aa7
“sandip5” <singhsandeep.singh97@gmail.com>
2020-05-11 11:27:50 +0530

    Switch case

--- EmpWageAttendance.java
+++ EmpWageAttendance.java
@@ -4,30 +4,25 @@ public class EmpWageAttendance
 {
         public static void main(String[] args)
         {
-                    Random rand = new Random();
-                int IS_PART_TIME=1;
-                int IS_FULL_TIME=2;
+        	    Random rand = new Random();
+                int IS_FULL_TIME=1;
+                int IS_PART_TIME=2;
                 int EMP_RATE_PER_HRS=20;
                 int salary;
-                int EMP_HRS;
+                int EMP_HRS = 0;
                 int empCheck=rand.nextInt(3);
-                if (empCheck == IS_FULL_TIME)
-                {
-                                EMP_HRS=8;
-                }
-                else if(empCheck == IS_PART_TIME)
-                {
-                                EMP_HRS=4;
-                }
-                else
-                {
-                                EMP_HRS=0;
+                switch (empCheck) {
+                case 1:
+                  EMP_HRS=8;
+                  break;
+                case 2:
+                  EMP_HRS=4;
+                  break;
+                default:
+                  EMP_HRS=0;	
                 }
         salary=(EMP_RATE_PER_HRS*EMP_HRS);
         System.out.println(salary);
         }
 }
-
-
-
 
