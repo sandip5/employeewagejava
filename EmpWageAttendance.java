147bc9fa2555bbd38e08f3db845a0916edd3c595
“sandip5” <singhsandeep.singh97@gmail.com>
2020-05-12 13:35:24 +0530

    Refactor code UC Seven

--- EmpWageAttendance.java
+++ EmpWageAttendance.java
@@ -1,20 +1,16 @@
 import java.util.Random;
-
-public class EmpWageAttendance
-{
-	public static void main(String[] args)
+class EmpHrs{
+	public int getTotalEmpHrs()
 	{
 		Random rand = new Random();
 		final int IS_FULL_TIME = 1;
 		final int IS_PART_TIME = 2; 
-		int EMP_RATE_PER_HRS = 20;
 		int MAX_HRS_MONTH = 100;
 		int NUM_WORK_DAYS = 20;
 		
 		int totalWorkingDays = 0;
 		int totalEmpHrs = 0;
 		int EMP_HRS=0;
-		int totalSalary=0;
 		
 		while(totalEmpHrs < MAX_HRS_MONTH && totalWorkingDays < NUM_WORK_DAYS)
 		{
@@ -32,6 +28,18 @@ public class EmpWageAttendance
 			}
 			totalEmpHrs=totalEmpHrs+EMP_HRS;
 		}	
+		return totalEmpHrs;
+	}
+}
+public class EmpWageAttendance
+{
+	public static void main(String[] args)
+	{
+		int EMP_RATE_PER_HRS = 20;
+		int totalSalary=0;
+		EmpHrs object = new EmpHrs();
+		int totalEmpHrs =object.getTotalEmpHrs();
+		System.out.println(totalEmpHrs);
 		totalSalary=totalEmpHrs*EMP_RATE_PER_HRS;
 		System.out.println(totalSalary);
 	}
